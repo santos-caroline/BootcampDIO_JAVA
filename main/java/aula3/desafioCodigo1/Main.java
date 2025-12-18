2ec2669623954144581deb18b3f357053a18c11e
carooull <carooull.1412@gmail.com>
2025-12-18 10:30:55 -0300

    add resolução do desafio de código 1

---
+++ main/java/aula3/desafioCodigo1/Main.java
@@ -0,0 +1,19 @@
+package aula3.desafioCodigo1;
+import java.util.Scanner;
+
+public class Main {
+    // resolução sem os códigos teste
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String solicitacao = scanner.nextLine();
+
+        String texto = solicitacao.toLowerCase();
+
+        if(solicitacao.contains("servidor para rodar")) System.out.println("EC2");
+        else if (solicitacao.contains("armazenar")) System.out.println("S3");
+        else if (solicitacao.contains("banco de dados")) System.out.println("RDS");
+        else if (solicitacao.contains("sem servidor")) System.out.println("Lambda");
+        else System.out.println("Servico desconhecido");
+    }
+}
